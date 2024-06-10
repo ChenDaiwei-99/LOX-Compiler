7e89c2eea4372af0c2c86504cb08f1fa1620df04
ChenDaiwei-99 <daiwei.chen2020@outlook.com>
2024-06-10 08:57:45 -0500

    feat: complete 9.4 (while loop)

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -177,6 +177,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
+        return null;
+    }
+
+    @Override
     public Void visitVarStmt(Stmt.Var stmt) {
         Object value = null;
         if (stmt.initializer != null) value = evaluate(stmt.initializer);
